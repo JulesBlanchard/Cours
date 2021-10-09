31e5c9130562e9747fdd351949b1739406f385bc
Jules Blanchard <90675105+JulesBlanchard@users.noreply.github.com>
2021-10-09 18:24:43 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,18 @@
+/*
+
+
+
+
+*/
+
+function myFunction() {
+  let bouton = document.getElementById("bouton");
+    bouton.innerHTML = "Eh beh non petit coquin, tu ne seras pas spoil&#129313 &#x1F92A";
+  bouton.style.fontSize = "25px"; 
+  bouton.style.color = "#C500FF"; 
+}
+
+
+
+
+
