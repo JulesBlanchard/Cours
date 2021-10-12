7edfb3b389fea233ffb7202d2ae280fdb317c35c
Jules Blanchard <90675105+JulesBlanchard@users.noreply.github.com>
2021-10-12 19:05:33 +0200

    Update java.js

--- java.js
+++ java.js
@@ -5,13 +5,21 @@
 
 */
 
-function myFunction() {
-  let bouton = document.getElementById("bouton");
-    bouton.innerHTML = "Eh beh non petit coquin, tu ne seras pas spoil &#129313 &#x1F92A";
-  bouton.style.fontSize = "25px"; 
-  bouton.style.color = "#C500FF"; 
-}
+let bouton = document.getElementById("bouton"); 
+bouton.style.fontSize = "25px"; 
+bouton.style.color = "#C500FF"; 
+var mode = "caché";
+function myFunction(){
+  if(mode =="caché"){
+    document.getElementById("bouton").innerHTML="Spoiler Alert !!!";
+    mode="ouvert";
+  }else {
+    document.getElementById("bouton").innerHTML="Eh beh non petit coquin, tu ne seras pas spoil &#129313 &#x1F92A";
+    mode="caché";
+    bouton.style.color = "#C500FF";
 
+  }
+}
 
 
 
