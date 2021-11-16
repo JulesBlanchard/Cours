c313dd1ebd24fbee3a4a46f8fd091aad525cf14e
Jules Blanchard <90675105+JulesBlanchard@users.noreply.github.com>
2021-11-16 21:33:34 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,16 @@
 
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('.navbar');
+var lineOne = document.querySelector('.navbar .menu-btn .line--1');
+var lineTwo = document.querySelector('.navbar .menu-btn .line--2');
+var lineThree = document.querySelector('.navbar .menu-btn .line--3');
+var link = document.querySelector('.navbar .nav-links');
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
